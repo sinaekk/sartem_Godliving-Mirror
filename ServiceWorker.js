8c9e9457ac0ac21f0ee682dd4f5b79acf66de408
sinaekk <sinae08@hanmail.net>
2024-12-31 14:46:18 +0900

    url수정

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "FutureVision-GodLiving-0.1";
+const contentToCache = [
+    "Build/24.12.31.godliving.loader.js",
+    "Build/24.12.31.godliving.framework.js",
+    "Build/24.12.31.godliving.data",
+    "Build/24.12.31.godliving.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
